047914a5f4dd7ca4b197d268915740cac116e6b4
Welton Leite <weltonrcc@hotmail.com>
2023-07-31 10:00:09 -0300

    Criado projeto sem classes

---
+++ src/main/java/com/algaworks/algafood/AlgafoodApi2Application.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafoodApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgafoodApi2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/algafood/AlgafoodApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodApi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
